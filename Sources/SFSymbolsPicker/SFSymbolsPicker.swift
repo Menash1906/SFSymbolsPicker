b7439dd43941286866ab3b214e03985026fb6ab7
Menash1906 <Menashyt@gmail.com>
2023-03-29 12:48:31 +0300

    first configurition

--- Sources/SFSymbolsPicker/SFSymbolsPicker.swift
+++ Sources/SFSymbolsPicker/SFSymbolsPicker.swift
@@ -1,6 +1,64 @@
-public struct SFSymbolsPicker {
-    public private(set) var text = "Hello, World!"
+//
+//  SFSymbolsPicker.swift
+//  AntiMess
+//
+//  Created by Michael Menashe on 29/03/2023.
+//
 
-    public init() {
+import SwiftUI
+
+struct SFSymbolsPicker: View {
+    
+    @Binding public var icon: String
+    @State var category: Category = .arrows
+    @State var searchText = ""
+
+    let columns = [GridItem(.adaptive(minimum: 70)), GridItem(.adaptive(minimum: 70)), GridItem(.adaptive(minimum: 70))]
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        VStack {
+            Picker("Category", selection: $category) {
+                ForEach(Category.allCases, id: \.id) { category in
+                    Text(category.rawValue == "" ? "All" : category.rawValue)
+                        .tag(category)
+                }
+            }
+            .pickerStyle(.menu)
+            ScrollView {
+                
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(searchText != "" ? symbols[category.rawValue]!.filter{$0.replacingOccurrences(of: ".", with:" ") .contains(searchText.lowercased())} : symbols[category.rawValue]!, id: \.hash) { icon in
+                        
+                        VStack {
+                            Image(systemName: icon)
+                                .font(.system(size: 25))
+                                .foregroundColor(self.icon == icon ? Color.blue : Color.primary)
+                            Text(icon.replacingOccurrences(of: ".", with: " "))
+                                .font(.caption)
+                                .multilineTextAlignment(.center)
+                        }
+                        .onTapGesture {
+                            // Assign binding value
+                            withAnimation(.linear) {
+                                self.icon = icon
+                                dismiss()
+                            }
+                        }
+                        
+                    }.padding(.top, 5)
+                }
+            }
+            .searchable(text: $searchText)
+            .frame(maxWidth: .infinity)
+        }
+        
+    }
+}
+
+struct SFSymbolsPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            SFSymbolsPicker(icon: .constant("gear"), category: .games)
+        }
     }
 }
